d83950f3ed7beccbfef0d08eb5e6a845bb6fef74
shashi-iitr <shashikrishna.sk@gmail.com>
2013-10-07 11:52:18 +0530

    modified

--- src/Arrays/GivenEleForMajorityEle.java
+++ src/Arrays/GivenEleForMajorityEle.java
@@ -1,23 +1,34 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class GivenEleForMajorityEle {
 
 	public static void main(String[] args){
-		int[]A={1, 2, 3, 3, 3, 3, 10};
+		int[]A={2, 2, 3, 3, 3, 3, 10};
 		int x=3, l=A.length;
 		int j=searchEleX(A, 0, l-1, x);
 		if(j==-1){
-			System.out.print("x is not a major element");
+			System.out.println("x is not a major element");
 		}
 		if(isExistX(A, j, l-1, x)){
-			System.out.print("x is major element");
+			System.out.println("x is major element");
 		} else
-			System.out.print("x is not a major element");
+			System.out.println("x is not a major element");
+		
+		if(xMajor(A, x, l-1)){
+			System.out.println("x is major element");
+		} else
+			System.out.println("x is not a major element");
+		
 		
 	}
 	
 	static boolean isExistX(int []A, int i, int l, int x){
-		int k=i+l/2;
+		int k=i+l/2-1;
 		if(k<=l && A[k]==x){
 			return true;
 		} else
@@ -36,5 +47,31 @@ public class GivenEleForMajorityEle {
 		}
 		else
 			return searchEleX(A, i, mid-1, x);
+	}
+	
+	
+	//O(n)
+	static boolean xMajor(int [] A, int x, int l){
+		HashMap<Integer, Integer> aMap=new  HashMap<Integer,Integer >();
+		for(int a:A){
+			int val=0;
+			if(aMap.containsKey(a)){
+				val=aMap.get(a);
+			}
+			val++;
+			aMap.put(a, val);
+		}
+		
+		Iterator it=aMap.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry ele=(Entry) it.next();
+			int value=(int) ele.getValue();
+			int key=(int) ele.getKey();
+			if(key==x && value>l/2){
+				return true;
+			}
+		}
+		return false;
+		
 	}
 }
